93d92618b9c09b410b4fd792b6a8ff6496836514
Deepak Mane <deepak.mane123@gmail.com>
2022-03-12 12:26:39 -0800

    Fix Palette Routes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
 import React, { Component } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useParams } from 'react-router-dom'
 import Palatte from './Palatte'
 import Home from './Home'
 import seedColors from './seedColors'
 import { generatePalette } from './colorHelpers'
-class App extends Component {
-  render() {
-    // console.log(generatePalette(seedColors[4]))
-    return (
-      <Routes>
-        <Route exact='true' path='/' element={<Home />} />
-        <Route
-          exact='true'
-          path='/palette/:id'
-          element={
-            <Palatte palette={generatePalette(seedColors[1])} />
-          }
-        />
-        } />
-      </Routes>
-      // <div>
-      //   <Palatte palette={generatePalette(seedColors[1])} />
-      // </div>
-    )
+
+function App() {
+  const findPalette = id => {
+    console.log('Inside findPalette', id)
+    return seedColors.find(function (palette) {
+      return palette.id === id
+    })
   }
+
+  const PaletteWrapper = () => {
+    const { id } = useParams()
+    console.log('Inside PaletteWrapper', id)
+    const item = findPalette(id)
+    console.log('Inside PaletteWrapper', item.colors)
+    return <Palatte palette={generatePalette(item)} />
+  }
+
+  return (
+    <Routes>
+      <Route exact='true' path='/' element={<Home />} />
+      <Route
+        exact='true'
+        path='/palette/:id'
+        element={<PaletteWrapper />}
+      />
+    </Routes>
+    // <div>
+    //   <Palatte palette={generatePalette(seedColors[1])} />
+    // </div>
+  )
 }
 
 export default App
