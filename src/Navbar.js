95523cdd82b1e41b6ba3d3c10874124983cacd8d
Deepak Mane <deepak.mane123@gmail.com>
2022-03-12 07:39:24 -0800

    Add Select and Material-UI

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
+import Select from '@mui/material/Select'
 import Slider, { Range } from 'rc-slider'
 import 'rc-slider/assets/index.css'
 
 import './Navbar.css'
+import { MenuItem } from '@mui/material'
 class Navbar extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { format: 'hex' }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(e) {
+    this.setState({ format: e.target.value })
+    this.props.handleChange(e.target.value)
+  }
   render() {
     const { level, changeLevel } = this.props
+    const { format } = this.state
     return (
       <header className='Navbar'>
         <div className='logo'>
@@ -23,6 +36,15 @@ class Navbar extends Component {
               onAfterChange={changeLevel}
             />
           </div>
+        </div>
+        <div className='select-container'>
+          <Select value={format} onChange={this.handleChange}>
+            <MenuItem value='hex'>HEX - #ffffff</MenuItem>
+            <MenuItem value='rgb'>RGB - rgb(255,255,255)</MenuItem>
+            <MenuItem value='rgba'>
+              RGBA - rgb(255,255,255. 1.0)
+            </MenuItem>
+          </Select>
         </div>
       </header>
     )
