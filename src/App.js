400ae054dd1f30da008f6d106d7c737ad0948b30
Deepak Mane <deepak.mane123@gmail.com>
2022-03-12 10:35:56 -0800

    added react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
+import React, { Component } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import Palatte from './Palatte'
+import Home from './Home'
 import seedColors from './seedColors'
 import { generatePalette } from './colorHelpers'
-
-import React, { Component } from 'react'
-
 class App extends Component {
   render() {
     // console.log(generatePalette(seedColors[4]))
     return (
-      <div>
-        <Palatte palette={generatePalette(seedColors[1])} />
-      </div>
+      <Routes>
+        <Route exact='true' path='/' element={<Home />} />
+        <Route
+          exact='true'
+          path='/palette/:id'
+          element={
+            <Palatte palette={generatePalette(seedColors[1])} />
+          }
+        />
+        } />
+      </Routes>
+      // <div>
+      //   <Palatte palette={generatePalette(seedColors[1])} />
+      // </div>
     )
   }
 }
